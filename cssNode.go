82cf4e9fd1c6177019418da1d446493abea6fa4d
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-11-25 15:49:23 -0700

    build out css render

--- cssNode.go
+++ cssNode.go
@@ -1,15 +1,12 @@
 package main
 
+import "fmt"
+
 // CSSNode is a node for all style data
 type CSSNode struct {
 	BaseNode
 	Rules []*CSSRule
 	Scope string
-}
-
-// Type returns CSSType
-func (node *CSSNode) Type() NodeType {
-	return CSSType
 }
 
 // Parse extracts all css rules and applies the correct scope to them
@@ -39,6 +36,40 @@ func (node *CSSNode) Parse(ctx NodeContext) error {
 	return nil
 }
 
-func (node *CSSNode) Render() string {
-	return ""
+func (node *CSSNode) Render(file *File) (*File, []*File) {
+	fmt.Println("CSS RENDER:", node)
+	content := ""
+	for _, rule := range node.Rules {
+		content += rule.String()
+	}
+
+	ret := []*File{}
+	if file.Type == CSSFileType {
+		file.Content += content
+	} else {
+		ret = append(ret, &File{
+			Type:    CSSFileType,
+			Name:    "bundle",
+			Content: content,
+		})
+	}
+
+	return file, ret
+}
+
+func (node *CSSNode) Clone() Node {
+	clone := CSSNode{
+		BaseNode: BaseNode{data: node.Data(), attr: node.Attrs(), visible: node.Visible()},
+	}
+
+	for _, child := range node.Children() {
+		clone.AppendChild(child.Clone())
+	}
+
+	for _, rule := range node.Rules {
+		clone.Rules = append(clone.Rules, rule.clone())
+	}
+	clone.Scope = node.Scope
+
+	return &clone
 }
