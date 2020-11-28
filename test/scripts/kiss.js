634fa744f5b3b6b34f9919b3ecd968039afd9d5c
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-11-28 10:10:12 -0700

    update kiss.js imports

--- test/scripts/kiss.js
+++ test/scripts/kiss.js
@@ -1,8 +1,8 @@
-({KISSimport: "observer.js"})
-({KISSimport: "sp.js"})
-({KISSimport: "sp1.js"})
-({KISSimport: "sp2.js"})
-({KISSimport: "scripts/external.js", nocompile: true, nobundle: true})
+({KISSimport: "observer.js"});
+({KISSimport: "sp.js"});
+({KISSimport: "sp1.js"});
+({KISSimport: "sp2.js"});
+({KISSimport: "scripts/external.js", nocompile: true, nobundle: true});
 
 function doKiss() {
     console.log("did the kiss");
